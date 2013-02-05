847a4f8fb478be47ef30fdf054b79016a346525b
ciocan <ciocan@mit.edu>
2013-02-05 15:40:53 -0500

    Initial Commit

---
+++ InstanceGenerator/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  InstanceGenerator
+//
+//  Created by Dragos Ciocan on 2/5/13.
+//  Copyright (c) 2013 Dragos Ciocan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
